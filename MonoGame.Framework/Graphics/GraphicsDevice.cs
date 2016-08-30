ad2528cbd5eebe6114d2c807029949e0b9cb817c
RHY3756547 <rhy3756547@hotmail.com>
2016-08-30 04:43:26 +0100

    Greatly improve GraphicsResource Dispose performance.

--- MonoGame.Framework/Graphics/GraphicsDevice.cs
+++ MonoGame.Framework/Graphics/GraphicsDevice.cs
@@ -109,7 +109,7 @@ namespace Microsoft.Xna.Framework.Graphics
         // Use WeakReference for the global resources list as we do not know when a resource
         // may be disposed and collected. We do not want to prevent a resource from being
         // collected by holding a strong reference to it in this list.
-        private readonly List<WeakReference> _resources = new List<WeakReference>();
+        private readonly HashSet<WeakReference> _resources = new HashSet<WeakReference>();
 
 		// TODO Graphics Device events need implementing
 		public event EventHandler<EventArgs> DeviceLost;
@@ -469,7 +469,7 @@ namespace Microsoft.Xna.Framework.Graphics
                     // Dispose of all remaining graphics resources before disposing of the graphics device
                     lock (_resourcesLock)
                     {
-                        foreach (var resource in _resources.ToArray())
+                        foreach (var resource in (new List<WeakReference>(_resources)).ToArray())
                         {
                             var target = resource.Target as IDisposable;
                             if (target != null)
@@ -560,7 +560,7 @@ namespace Microsoft.Xna.Framework.Graphics
                 }
 
                 // Remove references to resources that have been garbage collected.
-                _resources.RemoveAll(wr => !wr.IsAlive);
+                _resources.RemoveWhere(wr => !wr.IsAlive);
             }
         }
 
